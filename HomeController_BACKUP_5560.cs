a38f735aad1601a5551e98963155a631b4496ccd
Anton Korolkov <Anton.Korolkov@sparkybit.local>
2019-01-21 14:35:39 +0200

    github

---
+++ HomeController_BACKUP_5560.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext dc = new DataContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            ViewBag.Data = dc.Tables.Select(c=>c.Name);
+                //   Comment 1
+<<<<<<< HEAD
+                 //  Hello new branch
+=======
+               // barnch master
+>>>>>>> master
+            return View();
+        }
+    }
+}
---
+++ HomeController_LOCAL_2564.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext dc = new DataContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            ViewBag.Data = dc.Tables.Select(c=>c.Name);
+                //   Comment 1
+                 //  Hello new branch
+            return View();
+        }
+    }
+}
